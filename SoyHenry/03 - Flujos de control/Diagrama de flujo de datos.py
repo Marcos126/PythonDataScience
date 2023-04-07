024eb9d4c2481b950d620981fea040b751dd6202
Marcos126 <natalemarcos06@gmail.com>
2023-04-07 06:33:41 -0300

    a

--- SoyHenry/03 - Flujos de control/Diagrama de flujo de datos.py
+++ SoyHenry/03 - Flujos de control/Diagrama de flujo de datos.py
@@ -1,9 +1,14 @@
+#Date 7/4/2023 6:30 AM
+#Declaracion de variables 
 a = 100
 b = 2
+#Estructura de control
+#Si a es mayor que 0
 if (a > 0): 
+    #Bucle mientras sea mayor que 0
     while (a > 0):
             a = a - b
-            print(a)
+    #este if tiene que estar dentro del while para que use su output, si no, no funciona
     if(a == 0): 
         print ('a es divisible por b')
     else: 
