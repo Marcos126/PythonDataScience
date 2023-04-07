a3dc9f3319c6c7838de122ab549dbc111d437653
Marcos126 <natalemarcos06@gmail.com>
2023-04-07 06:24:09 -0300

    windowsXD

---
+++ windowsXD.py
@@ -0,0 +1,10 @@
+nro = 100
+div = 2
+
+if (nro > 0):
+    while (nro > 1):
+        nro = nro - div
+    if (nro == 0):
+        print("nro se puede dividir por div")
+    else :
+        print("nro no se puede dividir por div")
